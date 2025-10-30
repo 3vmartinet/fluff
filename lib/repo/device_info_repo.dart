117df48b9dd4cb188f1d60853c43d6774b60f1ff
valentin.martinet <valentin.martinet@>
2025-10-30 20:44:00 +0100

    Use flutter_device_info_plus

--- lib/repo/device_info_repo.dart
+++ lib/repo/device_info_repo.dart
@@ -2,14 +2,13 @@ import 'dart:io';
 
 import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
-import 'package:system_info_plus/system_info_plus.dart';
+import 'package:flutter_device_info_plus/flutter_device_info_plus.dart';
 
 class DeviceInfoRepo {
-  late final DeviceInfoPlugin _plugin;
+  final DeviceInfoPlugin _plugin = DeviceInfoPlugin();
+  final FlutterDeviceInfoPlus _deviceInfo = const FlutterDeviceInfoPlus();
 
-  DeviceInfoRepo() {
-    _plugin = DeviceInfoPlugin();
-  }
+  DeviceInfoRepo();
 
   Future<String?> get id async {
     if (Platform.isAndroid) {
@@ -30,6 +29,17 @@ class DeviceInfoRepo {
     return null;
   }
 
-  Future<int?> get physicalMemoryMb async =>
-      await SystemInfoPlus.physicalMemory;
+  Future<(int availableMb, int totalMb)> get physicalMemoryMb async {
+    final info = (await _deviceInfo.getDeviceInfo()).memoryInfo;
+
+    return (
+      info.availablePhysicalMemoryMB ~/ 1,
+      info.totalPhysicalMemoryMB ~/ 1
+    );
+  }
+
+  Future<(int coreCount, int maxFreq)> get processorInfo async {
+    final info = (await _deviceInfo.getDeviceInfo()).processorInfo;
+    return (info.coreCount, info.maxFrequency);
+  }
 }
