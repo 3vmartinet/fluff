7fc7bd2f2f36e10d0c6b01b833e1cec353fce247
3vmartinet <mofx71@gmail.com>
2025-11-17 22:33:50 +0100

    Extend VolumeRepo API

--- lib/repo/volume_repo.dart
+++ lib/repo/volume_repo.dart
@@ -1,10 +1,7 @@
 import 'package:volume_watcher_plus/volume_watcher_plus.dart';
 
 class VolumeRepo {
-  static final VolumeRepo _instance = VolumeRepo._init();
-  factory VolumeRepo() => _instance;
-
-  VolumeRepo._init() {
+  VolumeRepo() {
     volume.then((value) => _muted = value == 0);
   }
 
@@ -12,6 +9,7 @@ class VolumeRepo {
   bool get muted => _muted;
 
   Future<double> get volume async => await VolumeWatcherPlus.getCurrentVolume;
+  Future<double> get max async => await VolumeWatcherPlus.getMaxVolume;
 
   void mute() => _muted = true;
   void unmute() => _muted = false;
@@ -20,5 +18,13 @@ class VolumeRepo {
     final volume = await VolumeWatcherPlus.getMaxVolume / 2;
     _muted = false;
     return await VolumeWatcherPlus.setVolume(volume);
+  }
+
+  int? addListener(Function(double) listener) {
+    return VolumeWatcherPlus.addListener(listener);
+  }
+
+  void removeListener(int id) {
+    VolumeWatcherPlus.removeListener(id);
   }
 }
