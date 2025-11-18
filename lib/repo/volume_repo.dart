afc0b2c7d6203d971c3402c4312f76b8a7d99147
3vmartinet <mofx71@gmail.com>
2025-11-18 05:11:09 +0100

    Add logging to VolumeRepo's muted state

--- lib/repo/volume_repo.dart
+++ lib/repo/volume_repo.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:volume_watcher_plus/volume_watcher_plus.dart';
 
 class VolumeRepo {
@@ -6,13 +8,23 @@ class VolumeRepo {
   }
 
   bool _muted = false;
-  bool get muted => _muted;
+  bool get muted {
+    log("Requested mute state, answer muted = $_muted");
+    return _muted;
+  }
 
   Future<double> get volume async => await VolumeWatcherPlus.getCurrentVolume;
   Future<double> get max async => await VolumeWatcherPlus.getMaxVolume;
 
-  void mute() => _muted = true;
-  void unmute() => _muted = false;
+  void mute() {
+    _muted = true;
+    log("Mute volume");
+  }
+
+  void unmute() {
+    _muted = false;
+    log("Unmute volume");
+  }
 
   Future<bool> setAverageVolume() async {
     final volume = await VolumeWatcherPlus.getMaxVolume / 2;
