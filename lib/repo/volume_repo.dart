e7c1584238aaaabfdaad7e24c1613327b774b070
valentin.martinet <valentin.martinet@>
2025-11-28 10:56:55 +0100

    Extract VolumeRepo setup logic from constructor

--- lib/repo/volume_repo.dart
+++ lib/repo/volume_repo.dart
@@ -1,42 +1,23 @@
-import 'dart:developer';
-
 import 'package:volume_watcher_plus/volume_watcher_plus.dart';
 
 class VolumeRepo {
-  VolumeRepo() {
-    volume.then((value) => _muted = value == 0);
-  }
+  Future<double> get volume async => await VolumeWatcherPlus.getCurrentVolume;
 
-  bool _muted = false;
-  bool get muted {
-    log("Requested mute state, answer muted = $_muted");
+  Future<bool> initMutedState() async {
+    final currentVolume = await volume;
+    _muted = currentVolume == 0;
     return _muted;
   }
 
-  Future<double> get volume async => await VolumeWatcherPlus.getCurrentVolume;
-  Future<double> get max async => await VolumeWatcherPlus.getMaxVolume;
+  bool _muted = false;
+  bool get muted => _muted;
 
-  void mute() {
-    _muted = true;
-    log("Mute volume");
-  }
-
-  void unmute() {
-    _muted = false;
-    log("Unmute volume");
-  }
+  void mute() => _muted = true;
+  void unmute() => _muted = false;
 
   Future<bool> setAverageVolume() async {
     final volume = await VolumeWatcherPlus.getMaxVolume / 2;
     _muted = false;
     return await VolumeWatcherPlus.setVolume(volume);
-  }
-
-  int? addListener(Function(double) listener) {
-    return VolumeWatcherPlus.addListener(listener);
-  }
-
-  void removeListener(int id) {
-    VolumeWatcherPlus.removeListener(id);
   }
 }
