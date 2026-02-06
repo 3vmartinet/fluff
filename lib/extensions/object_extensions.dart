a585b3838447176cd835b3725fb853c464d8627f
valentin.martinet <valentin.martinet@...>
2026-02-06 21:29:56 +0100

    Avoid string interpolation in log statement, restrict to debug mode

--- lib/extensions/object_extensions.dart
+++ lib/extensions/object_extensions.dart
@@ -1,16 +1,28 @@
 import 'dart:developer';
+import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
 extension ObjectExtensions<T> on T {
-  void logInfo(String message) => _log(message, Level.INFO);
-  void logSevere(String message) => _log(message, Level.SEVERE);
-  void logShout(String message) => _log(message, Level.SHOUT);
-  void logWarning(String message) => _log(message, Level.WARNING);
-  void logFine(String message) => _log(message, Level.FINE);
-  void logFiner(String message) => _log(message, Level.FINER);
-  void logFinest(String message) => _log(message, Level.FINEST);
-  void logConfig(String message) => _log(message, Level.CONFIG);
+  void logInfo(String Function() messageProvider) =>
+      _log(messageProvider, Level.INFO);
+  void logSevere(String Function() messageProvider) =>
+      _log(messageProvider, Level.SEVERE);
+  void logShout(String Function() messageProvider) =>
+      _log(messageProvider, Level.SHOUT);
+  void logWarning(String Function() messageProvider) =>
+      _log(messageProvider, Level.WARNING);
+  void logFine(String Function() messageProvider) =>
+      _log(messageProvider, Level.FINE);
+  void logFiner(String Function() messageProvider) =>
+      _log(messageProvider, Level.FINER);
+  void logFinest(String Function() messageProvider) =>
+      _log(messageProvider, Level.FINEST);
+  void logConfig(String Function() messageProvider) =>
+      _log(messageProvider, Level.CONFIG);
 
-  void _log(String message, Level level) =>
-      log(message, name: runtimeType.toString(), level: level.value);
+  void _log(String Function() messageProvider, Level level) {
+    if (kDebugMode) {
+      log(messageProvider(), name: runtimeType.toString(), level: level.value);
+    }
+  }
 }
