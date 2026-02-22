eb7ad0ad5b3cc1ac7eea6578499caaebcf24972f
valentin.martinet <valentin.martinet@...>
2026-02-22 18:28:23 +0100

    Simplify SoundRepo

--- lib/repo/sound/sound_repo.dart
+++ lib/repo/sound/sound_repo.dart
@@ -5,8 +5,6 @@ import 'package:flutter_soloud/flutter_soloud.dart';
 
 class SoundRepo {
   final Map<String, AudioSource> _sounds = {};
-  final Set<SoundHandle> _activeHandles = {};
-  final Map<SoundHandle, StreamSubscription> _subscriptions = {};
 
   Future<void> init(List<String> assets) async {
     try {
@@ -23,39 +21,14 @@ class SoundRepo {
     }
   }
 
-  Future<void> play(String assetName, {double volume = 0.6}) async {
+  void play(String assetName, {double volume = 1.0}) {
     final source = _sounds[assetName];
     if (source != null) {
-      final handle = await SoLoud.instance.play(source, volume: volume);
-      _activeHandles.add(handle);
-
-      late final StreamSubscription sub;
-      sub = source.soundEvents.listen((event) {
-        if (event.handle == handle &&
-            event.event == SoundEventType.handleIsNoMoreValid) {
-          _activeHandles.remove(handle);
-          _subscriptions.remove(handle);
-          sub.cancel();
-        }
-      });
-      _subscriptions[handle] = sub;
+      SoLoud.instance.play(source, volume: volume);
     }
-  }
-
-  void stop() {
-    for (final sub in _subscriptions.values) {
-      sub.cancel();
-    }
-    _subscriptions.clear();
-
-    for (final handle in _activeHandles) {
-      SoLoud.instance.stop(handle);
-    }
-    _activeHandles.clear();
   }
 
   void dispose() {
-    stop();
     for (final source in _sounds.values) {
       SoLoud.instance.disposeSource(source);
     }
