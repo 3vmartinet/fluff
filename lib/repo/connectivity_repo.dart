d8a4e8ffdde79840d13cd64aeb4ba6de20ba9fad
valentin.martinet <valentin.martinet@...>
2026-03-24 10:10:31 +0100

    Fix hasConnectivity()

--- lib/repo/connectivity_repo.dart
+++ lib/repo/connectivity_repo.dart
@@ -2,9 +2,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 class ConnectivityRepo {
   Future<bool> hasConnectivity() async {
-    final connectivityResult = await Connectivity().checkConnectivity();
-
-    return connectivityResult.contains(ConnectivityResult.mobile) ||
-        connectivityResult.contains(ConnectivityResult.wifi);
+    final results = await Connectivity().checkConnectivity();
+    return results.firstOrNull != ConnectivityResult.none;
   }
 }
