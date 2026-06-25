7dbe7530b932a230a1ec1ef747a66b0f16f866c8
valentin.martinet <valentin.martinet@...>
2026-06-25 17:42:42 +0200

    Add unsaved AudioRepo

--- lib/repo/audio/audio_repo.dart
+++ lib/repo/audio/audio_repo.dart
@@ -3,5 +3,7 @@ abstract class AudioRepo {
   Future<void> cache(List<String> assetPaths);
   Future<void> play(String assetPath, {bool loop = false});
   Future<void> stop(String assetPath);
+  Future<void> pause(String assetPath);
+  Future<void> resume(String assetPath);
   Future<void> dispose();
 }
